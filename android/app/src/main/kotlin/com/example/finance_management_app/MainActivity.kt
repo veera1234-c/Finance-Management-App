a738f970dd14387a50f119c86021600d6451f91c
@veera1234-c <cveerasai01@gmail.com>
2023-01-24 20:03:21 +0530

    created flutter project file

---
+++ android/app/src/main/kotlin/com/example/finance_management_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.finance_management_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: SafeArea(child: Text("data")),
+    ),
+  ));
+}
