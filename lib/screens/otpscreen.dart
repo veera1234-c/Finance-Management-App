4f3c924e8109d2cbc9c6b4efa54e3e2c0ba6bf6a
@veera1234-c <cveerasai01@gmail.com>
2023-02-25 07:54:16 +0530

    changed

--- lib/screens/otpscreen.dart
+++ lib/screens/otpscreen.dart
@@ -5,78 +5,87 @@ import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 
 import '../constants/routes.dart';
 
-class OtpScreen extends StatelessWidget {
+class OtpScreen extends StatefulWidget {
+  //final String phone;
   const OtpScreen({super.key});
 
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.37,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage("assets/otpvector1.png"),
-                    ),
-                  ),
-                ),
-                Image.asset("assets/otplogo.png"),
-                OtpTextField(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  numberOfFields: 6,
-                  // focusedBorderColor: MyColors.ThemeColor,
-                  // borderWidth: 5,
-                  borderRadius: BorderRadius.circular(30),
-                  fillColor: Colors.black.withOpacity(0.1),
-                  filled: true,
-                  fieldWidth: 60,
-                  onCodeChanged: (value) => print("Changed : " + value),
-                  onSubmit: (value) {
-                    print("The OTP code is " + value);
-                    Navigator.pushNamed(context, Routes.menuScreen);
-                  },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "PLEASE ENTER THE OTP",
-                  style:
-                      TextStyle(fontFamily: "Inter", color: Color(0xFF6057A6)),
-                ),
-                Stack(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height * 0.14,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: AssetImage("assets/otpvector2.png"),
-                        ),
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.37,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("assets/otpvector1.png"),
                       ),
                     ),
-                    Positioned(
-                        bottom: 20,
-                        left: 10,
-                        child: Text(
-                          "DIDN’T GET? | RESEND OTP",
-                          style: TextStyle(
-                            fontFamily: "Inter",
-                            fontSize: 10,
-                            color: Colors.white,
+                  ),
+                  Image.asset("assets/otplogo.png"),
+                  OtpTextField(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    numberOfFields: 6,
+                    // focusedBorderColor: MyColors.ThemeColor,
+                    // borderWidth: 5,
+                    borderRadius: BorderRadius.circular(30),
+                    fillColor: Colors.black.withOpacity(0.1),
+                    filled: true,
+                    fieldWidth: 40,
+                    onCodeChanged: (value) => print("Changed : " + value),
+                    onSubmit: (value) {
+                      print("The OTP code is " + value);
+                      Navigator.pushNamed(context, Routes.menuScreen);
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "PLEASE ENTER THE OTP",
+                    style: TextStyle(
+                        fontFamily: "Inter", color: Color(0xFF6057A6)),
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.14,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage("assets/otpvector2.png"),
                           ),
-                        )),
-                  ],
-                )
-              ],
+                        ),
+                      ),
+                      Positioned(
+                          bottom: 20,
+                          left: 10,
+                          child: Text(
+                            "DIDN’T GET? | RESEND OTP",
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 10,
+                              color: Colors.white,
+                            ),
+                          )),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
           //     child: Stack(
